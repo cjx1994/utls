732977f20221b5e34c49a5dda28699f8e963ccfd
chenjiaxiong <chenjiaxiong@kingsoft.com>
2016-10-16 17:05:07 +0800

    test

---
+++ ksyun.tools/setup.py
@@ -0,0 +1,30 @@
+#-*- encoding: UTF-8 -*-
+from setuptools import setup, find_packages
+"""
+打包的用的setup必须引入，
+"""
+ 
+VERSION = '0.1.1'
+ 
+setup(name='ksyun.tools',
+      version=VERSION,
+      description="a tiny and smart cli player of douyutv,ximalayad,anmu based on Python",
+      long_description='just enjoy',
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='python douyu danmu danmu.fm terminal',
+      author='cjxcloud',
+      author_email='cjxcloud@163.com',
+      url='https://github.com/twocucao/doumu.fm',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+        'requests',
+      ],
+      entry_points={
+        'console_scripts':[
+            'danmu.fm = danmufm.danmu:main'
+        ]
+      },
+)
